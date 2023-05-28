02c2bc06625b8738a8e7def1630ef8a54d17fab2
VAIO <VAIO@DESKTOP-3THEP72>
2023-05-28 12:34:44 +0500

    Github Project

---
+++ src/group/pkg2/bse203030/bse203127/a3/Group2BSE203030BSE203127A3.java
@@ -0,0 +1,18 @@
+
+package group.pkg2.bse203030.bse203127.a3;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class Group2BSE203030BSE203127A3 {
+
+    
+    public static void main(String[] args) {
+        login_foam frame= new login_foam();
+        frame.setVisible(true);
+        //frame.setSize(400,300);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setLocation(500, 200);
+        frame.setTitle("Login");
+    }
+    
+}
